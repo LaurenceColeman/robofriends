7d5b5d8d88fdd8b930715065e6ebaa6955a0280b
mug-handle <97315295+LaurenceColeman@users.noreply.github.com>
2022-10-23 14:07:00 +0100

    CardList rewritten to mapp cards within the return clause

--- src/CardList.js
+++ src/CardList.js
@@ -2,19 +2,20 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-    const cardsArray = robots.map((user, i) => {
-        return (
-            <Card 
-                key = { robots[i].id } // key should be unique, i is bad because its value would change if array was edited
-                id = { robots[i].id } 
-                name = { robots[i].name } 
-                email = { robots[i].email } 
-                />
-        );
-    })
     return (
         <div>
-            {cardsArray}
+            {
+                robots.map((user, i) => {
+                    return (
+                        <Card
+                            key={robots[i].id}
+                            id={robots[i].id}
+                            name={robots[i].name}
+                            email={robots[i].email}
+                        />
+                    );
+                })
+            }
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <CardList robots={robots}/>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
