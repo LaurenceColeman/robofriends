cb80d8b583462cbd6b15de95639f13bf6459eb91
mug-handle <97315295+LaurenceColeman@users.noreply.github.com>
2022-10-24 13:33:38 +0100

    fetch from API added to componentDidMount() method

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css'
 
 class App extends Component {
@@ -11,34 +10,33 @@ class App extends Component {
       robots: [],
       searchfield: ''
     }
-    console.log('constructor');
 	}
 
   componentDidMount() {
-    // console.log('check');
-    this.setState({ robots: robots})
-    console.log('componentDidMount');
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
   }
 
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value })
-    // console.log(event.target.value);
   }
 
   render() {
     const filteredRobots = this.state.robots.filter(robots => {
       return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-    });
-    console.log('render');
-    // console.log(this.state.robots)
-    // console.log(filteredRobots);
-    return (
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filteredRobots} />   
-      </div>
-    );
+    })
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <CardList robots={filteredRobots} />   
+        </div>
+      );
+    }
   }
 }
 
